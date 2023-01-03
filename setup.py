30d1cfe2ca58a317d605f47e9615d9f8766c9448
Diego Vidaurre <dvidaurre@gmail.com>
2023-01-03 08:57:41 +0100

    changes to make the package ready

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='genephys',
+    version='0.1.1',
+    description='Generative model of empirical electrophysiological signals',
+    url='https://github.com/vidaurre/genephys',
+    author='Diego Vidaurre',
+    author_email = "dvidaurre@cfin.au.dk",
+    install_requires=['scipy','numpy','sklearn','matplotlib'],
+    packages=["genephys"],
+    zip_safe=False,
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3"]
+    )
+
